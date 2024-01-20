18cce1c111a5e2fadec01446c8fcd7acb055cbb9
regtan <regtan1210@gmail.com>
2024-01-20 21:55:30 +0900

    Add tests and view logic for future question handling

--- polls/tests.py
+++ polls/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
 from .models import Question
+from http import HTTPStatus
 
 def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days = days)
@@ -53,3 +54,16 @@ class QuestionIndexViewTests(TestCase):
         past_question2 = create_question(question_text="Past question 2.", days=-5)
         response = self.client.get(reverse("polls:index"))
         self.assertQuerySetEqual(response.context["latest_question_list"], [past_question2, past_question1])
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text="Future question." ,days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+    
+    def test_past_question(self):
+        past_question = create_question(question_text="Past question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
